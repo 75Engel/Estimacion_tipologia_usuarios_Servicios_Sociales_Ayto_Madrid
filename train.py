4167ac18f24b4fcc2623ef19999f28f01a0d66a8
75Engel <javiertenorioruiz@gmail.com>
2022-09-24 20:51:54 +0200

    Actualizacion proyecto

--- train.py
+++ train.py
@@ -13,29 +13,30 @@ from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
 
-#var.tree_reg.fit(var.X_train, var.y_train)
-#fn.save_files(var.tree_reg)
-
-#var.rnd_clf.fit(var.X_train, var.y_train)
-#fn.save_files(var.rnd_clf)
+var.ada_clf.fit(var.X_train, var.y_train)
+fn.save_files(var.ada_clf)
 
 var.bag_clf.fit(var.X_train, var.y_train)
 fn.save_files(var.bag_clf)
 
-#var.ada_clf.fit(var.X_train, var.y_train)
-#fn.save_files(var.ada_clf)
+var.tree_reg.fit(var.X_train, var.y_train)
+fn.save_files(var.tree_reg)
 
-#var.gbct.fit(var.X_train, var.y_train)
-#fn.save_files(var.gbct)
+var.rnd_clf.fit(var.X_train, var.y_train)
+fn.save_files(var.rnd_clf)
 
-#var.logreg.fit(var.X_train, var.y_train)
-#fn.save_files(var.logreg)
+
+var.gbct.fit(var.X_train, var.y_train)
+fn.save_files(var.gbct)
+
+var.logreg.fit(var.X_train, var.y_train)
+fn.save_files(var.logreg)
 
 #var.xgb_clas.fit(var.X_train, var.y_train)             #Revisar en  notebook
 #fn.save_files(var.xgb_clas)
 
-#scaler = MinMaxScaler()
-#X_train_scaler=scaler.fit_transform(var.X_train)
-#var.knn.fit(X_train_scaler, var.y_train)
-#fn.save_files(var.knn)
+scaler = MinMaxScaler()
+X_train_scaler=scaler.fit_transform(var.X_train)
+var.knn.fit(X_train_scaler, var.y_train)
+fn.save_files(var.knn)
 
